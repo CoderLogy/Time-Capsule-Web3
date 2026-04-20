0351fadff0f037000881bb9c301f88be25281ef8
Shourya wadhwa <71631493+CoderLogy@users.noreply.github.com>
2026-04-20 03:39:12 -0700

    Update cards.tsx

--- src/components/cards.tsx
+++ src/components/cards.tsx
@@ -2,7 +2,8 @@ import React, { useMemo, memo } from "react";
 import { Pill, Lock } from "lucide-react";
 import { Spinner } from "@/components/ui/spinner";
 import { Button } from "@/components/ui/button";
-import ViewCapsuleModal from "./ViewCapsuleModal.tsx";
+// @ts-ignore
+import ViewCapsuleModal from "./ViewCapsuleModal";
 import type { Capsule } from "@/lib/capsule-query";
 import { motion, AnimatePresence, useInView } from "framer-motion";
 
